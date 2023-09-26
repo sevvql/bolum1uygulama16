7e78a833696a91d090efc8151c81e973ae5dd325
sevvql <145970178+sevvql@users.noreply.github.com>
2023-09-26 11:51:18 +0300

    Add files via upload

---
+++ ornek7/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ornek7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double turkceDogru, matDogru;
+            double turkceYanlıs, matYanlıs;
+            double turkceNet, matNet;
+            turkceDogru = Convert.ToDouble(textBox1.Text);
+            turkceYanlıs = Convert.ToDouble(textBox2.Text);
+            turkceNet = (turkceDogru - (turkceYanlıs / 4));
+            textBox3.Text = turkceNet.ToString();
+            matDogru = Convert.ToDouble(textBox4.Text);
+            matYanlıs = Convert.ToDouble(textBox5.Text);
+            matNet = (matDogru - (matYanlıs / 4));
+            textBox6.Text = matNet.ToString();
+
+
+        }
+    }
+}
